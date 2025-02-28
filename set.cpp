725b3a03e7fd60dac1a5daa6f0d9355a33fdb55a
Talha Özer <talhaozr51@gmail.com>
2025-02-28 04:30:25 +0300

    setCpp

---
+++ set.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+
+int costumerId;
+set<int> costumerIdList;
+
+void xd() {
+    cout << "Enter costumer ID: ";
+    while (cin >> costumerId) {
+        if (costumerIdList.find(costumerId) != costumerIdList.end()) {
+            cout << "Costumer ID already exists. so your order will be delayed " << endl;
+        }
+        costumerIdList.insert(costumerId);
+        cout << "Current queue is :";
+        for (const auto &b : costumerIdList) {
+            cout << b << " ";
+        }
+        cout << endl << "Enter costumer ID: ";
+    }
+}
+
+int main() {
+    xd();
+    return 0;
+}
+
+
+
+
+
+
