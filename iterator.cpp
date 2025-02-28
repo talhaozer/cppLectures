28db59304f80f091cb1403dffb3923810682c5c7
Talha Özer <talhaozr51@gmail.com>
2025-02-28 06:25:05 +0300

    iteratorCpp

---
+++ iterator.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+  // Create a vector called cars that will store strings
+  vector<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+  // Loop through vector elements
+  for (auto it = cars.begin(); it != cars.end(); ) {
+    if (*it == "BMW") {
+      it = cars.erase(it); // Remove an element if it matches "BMW"
+    } else {
+      ++it;
+    }
+  }
+
+  // Print vector elements
+  for (const string& car : cars) {
+    cout << car << "\n";
+  }
+
+
+
+  map<string, int> people = { {"John", 32}, {"Adele", 45}, {"Bo", 29} };
+
+  // Loop through the map with an iterator
+  for (auto it = people.begin(); it != people.end(); ++it) {
+      cout << it->first << " is: " << it->second << "\n";
+  }
+
+
+
+
+
+  vector<int> numbers = {1, 7, 3, 5, 9, 2};
+
+  // Search for the number 3
+  auto it = find(numbers.begin(), numbers.end(), 3);
+
+  // Check if the number 3 was found
+  if (it != numbers.end()) {
+    cout << "The number 3 was found!" << "\n";
+  } else {
+    cout << "The number 3 was not found." << "\n";
+  }
+  
+
+
+
+  return 0;
+
+
+
+
+
+}
