cce7aae1d699e63364b8d994f7c691863e85ffaf
Talha Özer <talhaozr51@gmail.com>
2025-03-04 04:42:48 +0300

    rock.cpp

---
+++ rock.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+void playersChoise(int player1){
+
+    if(player1 == 1){
+        cout << "Player 1 chose Rock" << endl;
+    }else if(player1 == 2){
+        cout << "Player 1 chose Paper" << endl;
+    }else if(player1 == 3){
+        cout << "Player 1 chose Scissors" << endl;
+    }
+    else{
+        cout << "Invalid choice!" << endl;
+    }
+};
+
+void computersChoise(int computer){
+
+    if(computer == 1){
+        cout << "Computer chose Rock" << endl;
+    }else if(computer == 2){
+        cout << "Computer chose Paper" << endl;
+    }else if(computer == 3){
+        cout << "Computer chose Scissors" << endl;
+    }
+};
+
+void winner(int player1, int computer){
+
+    switch (player1)
+    {
+    case 1:
+        if(computer == 1){
+            cout << "It's a tie!" << endl;
+        }else if(computer == 2){
+            cout << "Computer wins!" << endl;
+        }else if(computer == 3){
+            cout << "Player 1 wins!" << endl;
+        }
+        break;
+    case 2:
+        if(computer == 1){
+            cout << "Player 1 wins!" << endl;
+        }else if(computer == 2){
+            cout << "It's a tie!" << endl;
+        }else if(computer == 3){
+            cout << "Computer wins!" << endl;
+        }
+        break;
+    case 3:
+        if(computer == 1){
+            cout << "Computer wins!" << endl;
+        }else if(computer == 2){
+            cout << "Player 1 wins!" << endl;
+        }else if(computer == 3){
+            cout << "It's a tie!" << endl;
+        }
+        break;      
+    default:
+        break;
+    }
+};
+
+int main(){
+    
+    cout << "Welcome to Rock, Paper, Scissors!" << endl;
+    cout << "Player 1, please choose your weapon: " << endl;
+    cout << "1  = Rock" << endl;
+    cout << "2 = Paper" << endl;
+    cout << "3 =  Scissors" << endl;
+
+
+    int player ;
+    cin >> player;
+    srand(time(0));
+    int computer = rand() % 3 + 1;
+
+    playersChoise(player);
+    computersChoise(computer);
+    winner(player, computer);
+
+   
+return 0;
+}
